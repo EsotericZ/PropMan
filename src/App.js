49e218d0392e14215ed74e06cbf08910be78cd54
EsotericZ <cjsand03@gmail.com>
2024-04-27 14:24:50 -0600

    adding authrequirements

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import { LoginButton } from "./components/LoginButton";
-import { LogoutButton } from "./components/LogoutButton";
-import { ProfileInfo } from "./components/ProfileInfo";
+// import { LoginButton } from "./components/LoginButton";
+// import { LogoutButton } from "./components/LogoutButton";
+// import { ProfileInfo } from "./components/ProfileInfo";
 
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages/home/Home";
+import { Layout } from "./pages/Layout";
 
 export const App = () => {
-    const { isLoading, error } = useAuth0();
+    // const { isLoading, error } = useAuth0();
 
+    // return (
+    //     <main>
+    //         <h1>Auth0 Login</h1>
+    //         {error && 
+    //             <p>Authentication Error</p>
+    //         }
+    //         {!error && isLoading && 
+    //             <p>Loading</p>
+    //         }
+    //         {!error && !isLoading && (
+    //             <>
+    //                 <LoginButton />
+    //                 <LogoutButton />
+    //                 <ProfileInfo />
+    //             </>
+    //         )}
+    //     </main>
+    // );
     return (
-        <main>
-            <h1>Auth0 Login</h1>
-            {error && 
-                <p>Authentication Error</p>
-            }
-            {!error && isLoading && 
-                <p>Loading</p>
-            }
-            {!error && !isLoading && (
-                <>
-                    <LoginButton />
-                    <LogoutButton />
-                    <ProfileInfo />
-                </>
-            )}
-        </main>
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                </Route>
+            </Routes>
+        </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from './App';
+import { App } from "./App";
 
 import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTHO_CLIENT_ID;
 
 ReactDOM.render(
     <React.StrictMode>
-        <Auth0Provider
-            domain={domain}
-            clientId={clientId}
-            redirectUri={window.location.origin}
-        >
-            <App />
-        </Auth0Provider>
+        <Router>
+            <Auth0Provider
+                domain={domain}
+                clientId={clientId}
+                redirectUri={window.location.origin}
+            >
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </Auth0Provider>
+        </Router>
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById("root")
 );
